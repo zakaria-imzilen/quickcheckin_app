4dbdeae289cf5ac69abfff3ffc0e43d8162975c4
Zakaria Imzilen <zakariaaskary15@gmail.com>
2023-05-02 00:16:48 +0100

    Initial Integration is Done

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>QuickCheckin</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        @viteReactRefresh
+        @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    </head>
+    <body class="antialiased">
+        <div id="root"></div>
+    </body>
+</html>
