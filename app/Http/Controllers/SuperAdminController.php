1baf906adb7efa118c7b92afdf3212397efbf77a
Zakaria Imzilen <zakariaaskary15@gmail.com>
2023-05-10 20:17:02 +0100

    Login method is completly functionning + generating auth code

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -7,12 +7,34 @@ use App\Models\SuperAdmin;
 
 class SuperAdminController extends Controller
 {
+    public function generate_auth_code($id)
+    {
+        $randomCode = rand(1000, 9999);
+        $auth_expiring = date('H:i:s', strtotime('+15 minutes'));
+
+        $result = SuperAdmin::where('id', $id)->update(['auth_code' => $randomCode, 'auth_expiring' => $auth_expiring]);
+
+        if ($result === 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public function login(Request $request)
     {
         // Check if there is a superadmin with the crendentials provided
-        // IF SO --> Generate a new Auth_Code
         $result = SuperAdmin::where('email', $request->email)->where('pwd', $request->pwd)->get();
-        return $result;
+        // IF SO --> Generate a new Auth_Code
+        if (count($result) === 1) {
+            if ($this->generate_auth_code($result[0]['id'])) {
+                return true;
+            } else {
+                abort(500, "Something went wrong with generating an authentication code");
+            }
+        } else {
+            return [];
+        }
     }
 
     public function checkCode(Request $request)
--- routes/superadmins.php
+++ routes/superadmins.php
@@ -4,4 +4,4 @@
 use App\Http\Controllers\SuperAdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('auth/login', [SuperAdminController::class, 'login']);
+Route::post('sa/auth/login', [SuperAdminController::class, 'login']);
