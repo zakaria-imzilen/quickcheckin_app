018019660b85e04895f28894cdbf13caf06ce5ca
Zakaria Imzilen <zakariaaskary15@gmail.com>
2023-05-10 10:33:35 +0100

    Event Controller is created + Its routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/events', [EventController::class, 'display']);
+
+Route::get('/events/{q}', [EventController::class, 'searchEvent']);
+
+Route::post('/events/add', [EventController::class, 'addEvent']);
+
+Route::post('/event/edit/{id}', [EventController::class, 'editEvent']);
 
 Route::get('/{any}', function () {
     return view('app');
