5addd665a4f1f7bf5efbcbe93f0dae7b4df9d112
Zakaria Imzilen <zakariaaskary15@gmail.com>
2023-05-12 13:07:24 +0100

    Organizers: Added 2 new methods (WORKING)

--- app/Http/Controllers/OrganizerController.php
+++ app/Http/Controllers/OrganizerController.php
@@ -39,4 +39,41 @@ class OrganizerController extends Controller
             ]);
         }
     }
+
+    public function logIn(Request $request)
+    {
+        $result = Organizer::where('email', $request->email)->where('pwd', $request->pwd)->get();
+
+        if (count($result) > 0) {
+            return json_encode([
+                "status" => true,
+                "data" => [
+                    "id" => $result[0]['id'],
+                    "firstName" => $result[0]['firstName'],
+                    "lastName" => $result[0]['lastName'],
+                    "email" => $result[0]['email'],
+                    "organizationName" => $result[0]['organizationName'],
+                ]
+            ]);
+        }
+
+        return json_encode([
+            "status" => false
+        ]);
+    }
+
+    public function edit(Request $request)
+    {
+        $result = Organizer::where('id', $request->id)->update($request->all());
+
+        if ($result === 1) {
+            return json_encode([
+                "updated" => true,
+            ]);
+        } else {
+            return json_encode([
+                "updated" => false,
+            ]);
+        }
+    }
 }
--- routes/organizers.php
+++ routes/organizers.php
@@ -7,3 +7,6 @@ Route::get('/sa/organizers/displayall', [OrganizerController::class, 'displayAll
 Route::get('/sa/organizers/search/{q}', [OrganizerController::class, 'search']);
 
 Route::post('/sa/organizers/signup', [OrganizerController::class, 'signUp']);
+Route::post('/sa/organizers/login', [OrganizerController::class, 'logIn']);
+
+Route::post('/sa/organizers/edit', [OrganizerController::class, 'edit']);
