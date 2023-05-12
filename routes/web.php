efe76aba365a7c308f909f5d24c0197482640563
Zakaria Imzilen <zakariaaskary15@gmail.com>
2023-05-12 18:43:27 +0100

    UserController: all meothods are there and working just fine

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ require('superadmins.php');
 require('organizers.php');
 // >>
 
+// << Use rRoutes
+require('users.php');
+// >>
+
 Route::get('/{any}', function () {
     return view('app');
 })->where('any', '.*');
