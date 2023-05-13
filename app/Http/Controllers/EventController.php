965dd28e54b85ba879e2e3bbe9a824b58b20988a
Zakaria Imzilen <zakariaaskary15@gmail.com>
2023-05-13 23:17:18 +0100

    EventController: Display method edited -> Events rendered with packages inside

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Models\EventPackage;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -10,8 +11,17 @@ class EventController extends Controller
     public function display($limit = 10, $skip = 0)
     {
         $event = new Event;
+        $events = $event->get()->skip($skip)->take($limit);
 
-        return $event->get()->skip($skip)->take($limit);
+        $toBeReturned = [];
+        foreach ($events as $eve) {
+            $packResult = EventPackage::where('eventId', $eve['id'])->get();
+            $eventObj = [$eve, "packages" => $packResult];
+
+            array_push($toBeReturned, $eventObj);
+        }
+
+        return json_encode($toBeReturned);
     }
 
     public function searchEvent(Request $request, $q)
