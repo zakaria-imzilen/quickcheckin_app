3b85d687a330348afab765c233652cb1af77a356
Zakaria Imzilen <zakariaaskary15@gmail.com>
2023-06-14 00:54:32 +0100

    Home Page: Changing cards styling

--- resources/js/components/EventsList.jsx
+++ resources/js/components/EventsList.jsx
@@ -60,28 +60,54 @@ const EventsList = ({ page }) => {
     return (
         <>
             {loading ? <Loading /> : ""}
-            <div className="my-10 flex justify-center gap-y-4 gap-x-2 flex-wrap max-w-screen-2xl mx-auto">
+            <div className="my-10 flex justify-center gap-y-4 gap-x-4 flex-wrap max-w-screen-2xl mx-auto">
                 {page === "home"
                     ? events.map(({ event }) => (
-                          <Link
-                              key={event.id}
-                              to={`/event/${event.slug}`}
-                              className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:gap-2 md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-                          >
-                              <img
-                                  className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                                  src={event.imageURL}
-                                  alt={event.name}
-                              />
-                              <div className="flex flex-col px-2 justify-between leading-normal">
-                                  <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                      {event.name}
-                                  </h5>
-                                  <p className="font-normal text-gray-700 dark:text-gray-400">
-                                      {event.description}
+                          <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-min">
+                              <Link to={`/event/${event.slug}`}>
+                                  <img
+                                      class="rounded-t-lg"
+                                      src={event.imageURL}
+                                      alt=""
+                                  />
+                              </Link>
+                              <div class="p-5 relative">
+                                  <div className="absolute top-0 right-0 px-4 py-2 text-xs font-sans uppercase bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-neutral-300 via-neutral-300 to-indigo-100 rounded-l-xl font-semibold">
+                                      {event.location} - {event.date}
+                                  </div>
+                                  <Link to={`/event/${event.slug}`}>
+                                      <h5 class="mb-2 mt-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                          {event.name}
+                                      </h5>
+                                  </Link>
+                                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                      {event.description.length > 70
+                                          ? event.description.slice(0, 70) +
+                                            "..."
+                                          : event.description}
                                   </p>
+                                  <Link
+                                      to={`/event/${event.slug}`}
+                                      class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                  >
+                                      Buy Now
+                                      <svg
+                                          xmlns="http://www.w3.org/2000/svg"
+                                          fill="none"
+                                          viewBox="0 0 24 24"
+                                          strokeWidth={1.5}
+                                          stroke="currentColor"
+                                          className="ml-3 w-4 h-4"
+                                      >
+                                          <path
+                                              strokeLinecap="round"
+                                              strokeLinejoin="round"
+                                              d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z"
+                                          />
+                                      </svg>
+                                  </Link>
                               </div>
-                          </Link>
+                          </div>
                       ))
                     : categoryEvents?.map((event) => (
                           <Link
